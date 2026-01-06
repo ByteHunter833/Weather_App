d240c461019a7b82a0e2a468172d07f89e9efb63
shukrullo <shukrulloqurbonov342@gmail.com>
2026-01-06 22:49:42 +0500

    Edited weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,7 +6,7 @@ import 'package:weather_app/models/weather_model.dart';
 class WeatherService {
   // Replace with your OpenWeatherMap API key
   // Get your free API key from: https://openweathermap.org/api
-  static const String apiKey = String.fromEnvironment('OPEN_WEATHER_API_KEY');
+  static const String apiKey = 'YOUR_API_KEY_HERE';
 
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
 
